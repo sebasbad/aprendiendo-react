ae17a2a745ed50e7abe56ae8f72147ddbbe33fd0
sebasbad <sebasbad@gmail.com>
2024-07-14 18:56:41 +0200

    Updated main to render more than one hola button in a react fragment

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const root  = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<button>Hola button</button>)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.Fragment>
+    <button>Hola button</button>
+    <button>Hola button</button>
+  </React.Fragment>
+)
