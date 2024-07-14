b14f484d030cb0a9020594f48c9c6c140d05944c
sebasbad <sebasbad@gmail.com>
2024-07-14 18:54:07 +0200

    Updated main to render a hola button

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root  = ReactDOM.createRoot(document.getElementById('root'))
-root.render("Hola mundo")
+root.render(<button>Hola button</button>)
