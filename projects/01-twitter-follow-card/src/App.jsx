227143f40b098472b9ee41c26910729c85b6348d
sebasbad <sebasbad@gmail.com>
2024-07-15 21:12:57 +0200

    Updated app jsx to add is following properties shorter version

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -4,10 +4,10 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 export function App () {
     return (
         <section className='App'>
-            <TwitterFollowCard isFollowing={true} userName="duckduckgo/gummibeer.dev" name="Gummi Beer Dev" />
+            <TwitterFollowCard isFollowing userName="duckduckgo/gummibeer.dev" name="Gummi Beer Dev" />
             <TwitterFollowCard isFollowing={false} userName="pikachu" name="Pi Ka Chu" />
-            <TwitterFollowCard userName="duckduckgo" name="Duck Duck Go" />
-            <TwitterFollowCard userName="brave" name="Brave" />
+            <TwitterFollowCard isFollowing userName="duckduckgo" name="Duck Duck Go" />
+            <TwitterFollowCard isFollowing userName="brave" name="Brave" />
         </section>
     )
 }
