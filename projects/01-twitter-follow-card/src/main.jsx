1577fe31c6dc1a3eba524f99df2405ccda8334f9
sebasbad <sebasbad@gmail.com>
2024-07-15 19:21:44 +0200

    Created and used an app jsx component

--- projects/01-twitter-follow-card/src/main.jsx
+++ projects/01-twitter-follow-card/src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { App } from './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<div>Twitter Card</div>)
+root.render(
+  <App />
+)
