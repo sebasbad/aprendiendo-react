d47f7de4fb0382ecbdf4a6cee2de79f7c010489c
sebasbad <sebasbad@gmail.com>
2024-07-15 20:51:10 +0200

    Updated app jsx to use empty react fragment element

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
     return (
-        <React.Fragment>
+        <>
             <TwitterFollowCard userName="duckduckgo/gummibeer.dev" name="Gummi Beer Dev" />
             <TwitterFollowCard userName="duckduckgo/gummibeer.dev" name="Gummi Beer Dev" />
             <TwitterFollowCard userName="duckduckgo/gummibeer.dev" name="Gummi Beer Dev" />
-        </React.Fragment>
+        </>
     )
 }
