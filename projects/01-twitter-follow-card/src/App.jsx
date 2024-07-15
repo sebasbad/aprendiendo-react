527858fdc5c35981d4bff08a3429c5900c088471
sebasbad <sebasbad@gmail.com>
2024-07-15 21:08:13 +0200

    Updated app jsx to add contained twitter cards separation style

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -3,10 +3,10 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
     return (
-        <>
+        <section className='App'>
             <TwitterFollowCard userName="duckduckgo/gummibeer.dev" name="Gummi Beer Dev" />
             <TwitterFollowCard userName="pikachu" name="Pi Ka Chu" />
             <TwitterFollowCard userName="duckduckgo" name="Duck Duck Go" />
-        </>
+        </section>
     )
 }
