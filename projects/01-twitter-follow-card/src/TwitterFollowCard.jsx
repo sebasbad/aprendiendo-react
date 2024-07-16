b250bddec12856662c30c50b4be72232ed755c46
sebasbad <sebasbad@gmail.com>
2024-07-16 23:11:37 +0200

    Updated app and twitter card to apply follow and unfollow dynamic button style and text

--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -29,7 +29,8 @@ export function TwitterFollowCard ({ children, userName = 'unknown', initialIsFo
             </header>
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text}
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
                 </button>
             </aside>
         </article>
