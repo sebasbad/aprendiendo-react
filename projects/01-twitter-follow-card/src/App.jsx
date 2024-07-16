eb4f74f4f5354913f5817e5bbeba10d0047df962
sebasbad <sebasbad@gmail.com>
2024-07-16 23:24:47 +0200

    Updated app to render a list of users from an array of users data

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -3,21 +3,43 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
+    const users = [
+        {
+            userName: 'duckduckgo/gummibeer.dev',
+            name: 'Gummi Beer Dev',
+            isFollowing: true
+        },
+        {
+            userName: 'pikachu',
+            name: 'Pi Ka Chu',
+            isFollowing: false
+        },
+        {
+            userName: 'duckduckgo',
+            name: 'Duck Duck Go',
+            isFollowing: true
+        },
+        {
+            name: 'Brave',
+            isFollowing: true
+        }
+    ]
 
     return (
         <section className='App'>
-            <TwitterFollowCard userName="duckduckgo/gummibeer.dev" initialIsFollowing>
-                Gummi Beer Dev
-            </TwitterFollowCard>
-            <TwitterFollowCard userName="pikachu" initialIsFollowing={false}>
-                Pi Ka Chu
-            </TwitterFollowCard>
-            <TwitterFollowCard userName="duckduckgo" initialIsFollowing>
-                Duck Duck Go
-            </TwitterFollowCard>
-            <TwitterFollowCard initialIsFollowing>
-                Brave
-            </TwitterFollowCard>
+            {
+                users.map(user => {
+                    const { userName, name, isFollowing} = user
+                    return(
+                        <TwitterFollowCard 
+                        userName={userName}
+                        isFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>        
+                    )
+                })
+            }
         </section>
     )
 }
