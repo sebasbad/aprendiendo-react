df87fb5b14ce32a85928d9be2d36780ca1bd80e5
sebasbad <sebasbad@gmail.com>
2024-07-16 23:32:33 +0200

    Updated app to refactor rennder of list of users and add key as something unique

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -28,17 +28,17 @@ export function App () {
     return (
         <section className='App'>
             {
-                users.map(user => {
-                    const { userName, name, isFollowing} = user
-                    return(
+                users.map( ({ userName, name, isFollowing}) => 
+                    (
                         <TwitterFollowCard 
-                        userName={userName}
-                        isFollowing={isFollowing}
-                    >
-                        {name}
-                    </TwitterFollowCard>        
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
                     )
-                })
+                )
             }
         </section>
     )
