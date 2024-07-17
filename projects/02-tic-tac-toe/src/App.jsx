5f11c4dab70f63a8b6c9b6fb162874f9b0202b44
sebasbad <sebasbad@gmail.com>
2024-07-17 19:45:01 +0200

    Updated app to check winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -13,23 +13,66 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   }
 
   return (
-    <div onClick={handleClick} className={className}>{children}</div>
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
   )
 }
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  // null: no hay ganador, false: empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    // para ver si X u O ganó
+    for(const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+
+    // si no hay ganador
+    return null
+  }
 
   const updateBoard = (index) => {
-    if(board[index]) {return}
+    // no actualizamos esta posición 
+    // si ya tiene algo o ya hay un ganador
+    if(board[index] || winner) {return}
 
+    // actualizar el tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
 
+    // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
