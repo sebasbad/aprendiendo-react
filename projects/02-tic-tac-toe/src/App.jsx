1fd32c3190a86728316bb73b5cfcd1a99a2d3a95
sebasbad <sebasbad@gmail.com>
2024-07-17 17:50:18 +0200

    Updated app to build first board iteration

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,7 +1,28 @@
-import './App.css'
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const board = Array(9).fill(null)
 
 function App() {
-  return <h1>Tic tac toe</h1>
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <div className="cell" key={index}>
+                <span className="cell__content">{index}</span>
+              </div>
+            )
+          })
+        }
+      </section>
+    </main>
+  )
+
 }
 
 export default App
