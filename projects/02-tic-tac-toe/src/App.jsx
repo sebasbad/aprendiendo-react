4254d3911874c8bc4a607e6b195228a02d3562e0
sebasbad <sebasbad@gmail.com>
2024-07-17 18:39:27 +0200

    Updated app to show board values

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,9 +1,9 @@
+import {useState} from 'react'
+
 const TURNS = {
   X: 'x',
   O: 'o'
 }
-
-const board = Array(9).fill(null)
 
 const Square = ({children, updateBoard, index}) => {
   return (
@@ -12,6 +12,8 @@ const Square = ({children, updateBoard, index}) => {
 }
 
 function App() {
+  const [board, setBoard] = useState(['x', 'x', 'x', 'o', 'o', 'o', 'x', 'o', 'x'])
+
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
@@ -23,7 +25,7 @@ function App() {
                 key={index}
                 index={index}
               >
-                {index}
+                {board[index]}
               </Square>
             )
           })
