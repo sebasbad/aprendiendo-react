b90289b9cdf993a0fb45792e37e2e4926b55b36f
sebasbad <sebasbad@gmail.com>
2024-07-17 17:57:05 +0200

    Updated app to build 2nd board iteration

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,6 +5,12 @@ const TURNS = {
 
 const board = Array(9).fill(null)
 
+const Square = ({children, updateBoard, index}) => {
+  return (
+    <div className="square">{children}</div>
+  )
+}
+
 function App() {
   return (
     <main className="board">
@@ -13,9 +19,12 @@ function App() {
         {
           board.map((_, index) => {
             return (
-              <div className="cell" key={index}>
-                <span className="cell__content">{index}</span>
-              </div>
+              <Square
+                key={index}
+                index={index}
+              >
+                {index}
+              </Square>
             )
           })
         }
