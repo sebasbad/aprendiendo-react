2ab95d560fafc4f322506484de82a331686c55b7
sebasbad <sebasbad@gmail.com>
2024-07-18 19:49:20 +0200

    Updated app to extract connstants to a separate js file

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,22 +1,7 @@
 import {useState} from "react"
 import connfetti from "canvas-confetti"
 import { Square } from "./components/Square"
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+import { TURNS, WINNER_COMBOS } from "./constants"
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
