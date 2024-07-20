6cbd18b96b821a39662135215e5dd1bac8b51538
sebasbad <sebasbad@gmail.com>
2024-07-20 22:47:00 +0200

    Updated app to prepare use effect and state code

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react'
+
 function App () {
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('effect', [enabled])
+  }, [enabled])
   return (
-    <h3>Proyecto 3</h3>
+    <>
+      <h3>Proyecto 3</h3>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
   )
 }
 
