1a0a74c0ca307561064818b7f3dea57c0b7fa040
sebasbad <sebasbad@gmail.com>
2024-07-21 16:54:41 +0200

    Updated app to add div with style

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -7,12 +7,24 @@ function App () {
     console.log('effect', [enabled])
   }, [enabled])
   return (
-    <>
-      <h3>Proyecto 3</h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#696',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'translate(0px, 0px)'
+      }}
+      />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
-    </>
+    </main>
   )
 }
 
