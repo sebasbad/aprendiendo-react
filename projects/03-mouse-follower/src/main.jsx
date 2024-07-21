15bda6e51154aa16481c2ea3e73f66ad53f1a9db
sebasbad <sebasbad@gmail.com>
2024-07-21 18:44:13 +0200

    Updated main to add comment about the react strictmode component

--- projects/03-mouse-follower/src/main.jsx
+++ projects/03-mouse-follower/src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // The React.StrictMode component helps in development mode
+  // to detect code warnings and errors, e.g.:
+  // -> executes the effects and then the cleanup to detect if
+  //    it's coded properly
   <React.StrictMode>
     <App />
   </React.StrictMode>
