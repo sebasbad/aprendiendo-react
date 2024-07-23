b44d9b1791fa0e473b5f63c3ae20ac84002e5389
sebasbad <sebasbad@gmail.com>
2024-07-23 21:34:25 +0200

    Updated app to add state

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,12 @@
+import { useState } from 'react'
+
 export function App () {
+  const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+
   return (
-    <h1>App de gatitos</h1>
+    <main>
+      <h1>App de gatitos</h1>
+      <p>{fact}</p>
+    </main>
   )
 }
