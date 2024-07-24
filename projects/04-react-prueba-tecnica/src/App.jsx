d37fddd491da88e52221c0b2ebd0d22cd23e8187
sebasbad <sebasbad@gmail.com>
2024-07-24 18:38:44 +0200

    Updated app to call catfact api

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,7 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function App () {
   const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+
+  // Importante conocer fetch pq a veces no se permite usar
+  // React Query, SWR, axios, apollo
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, /* la primera vez sólo */ [])
 
   return (
     <main>
