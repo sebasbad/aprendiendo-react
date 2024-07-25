b4527e98191d758ebf9a7fc71cd5536383881952
sebasbad <sebasbad@gmail.com>
2024-07-25 22:01:07 +0200

    Updated cat fact hook to use function

--- projects/04-react-prueba-tecnica/src/hooks/useCatFact.js
+++ projects/04-react-prueba-tecnica/src/hooks/useCatFact.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getRandomFact } from '../services/facts'
 
-export const useCatFact = () => {
+export function useCatFact () {
   const [fact, setFact] = useState()
 
   const refreshFact = () => {
