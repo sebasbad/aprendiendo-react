ff88f86cf9be7e591d8f6ce79f5bb37808291836
sebasbad <sebasbad@gmail.com>
2024-07-27 22:54:39 +0200

    Updated app to improve movies rendering by extracting render functions; easier to read but not a good practice

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -6,6 +6,28 @@ function App () {
   const movies = withResults.Search
   const hasMovies = movies?.length > 0
 
+  const renderMovies = () => {
+    return (
+      <ul>
+        {
+              movies.map(movie => (
+                <li key={movie.imdbID}>
+                  <h3>{movie.Title}</h3>
+                  <p>{movie.Year}</p>
+                  <img src={movie.Poster} alt={movie.Title} />
+                </li>
+              ))
+            }
+      </ul>
+    )
+  }
+
+  const renderNoResults = () => {
+    return (
+      <p>No se encontraron película para esta búsqueda</p>
+    )
+  }
+
   return (
     <div className='page'>
       <header>
@@ -18,22 +40,8 @@ function App () {
       <main>
         {
         hasMovies
-          ? (
-            <ul>
-              {
-              movies.map(movie => (
-                <li key={movie.imdbID}>
-                  <h3>{movie.Title}</h3>
-                  <p>{movie.Year}</p>
-                  <img src={movie.Poster} alt={movie.Title} />
-                </li>
-              ))
-            }
-            </ul>
-            )
-          : (
-            <p>No se encontraron película para esta búsqueda</p>
-            )
+          ? renderMovies()
+          : renderNoResults()
        }
       </main>
     </div>
