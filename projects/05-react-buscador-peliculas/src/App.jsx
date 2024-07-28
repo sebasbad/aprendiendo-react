f005bc65378511fa507ed429fb596cdcddd943a1
sebasbad <sebasbad@gmail.com>
2024-07-28 16:55:35 +0200

    Updated app to import use ref hook and add description

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,4 +1,10 @@
 import './App.css'
+// useRef es un hook que permite crear una referencia mutable que persiste
+// durante todo el lifecycle del componente; muy útil para guardar datos
+// mutables como un id, un elemento del DOM, un contador, etc. y que cada
+// vez que cambia NO vuelve a renderizar el componente, NO dispara un nuevo
+// renderizado
+import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
