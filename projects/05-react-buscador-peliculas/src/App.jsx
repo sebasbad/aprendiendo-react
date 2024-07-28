f6cccda80f38a30e0af8d4cbd90dd21bffb9e91d
sebasbad <sebasbad@gmail.com>
2024-07-28 16:35:39 +0200

    Updated app to extract use movies to a hook file

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,19 +1,6 @@
 import './App.css'
-import withResults from './mocks/with-results.json'
-import noResults from './mocks/no-results.json'
 import { Movies } from './components/Movies'
-
-export function useMovies () {
-  const movies = withResults.Search
-  const mappedMovies = movies?.map(movie => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
-
-  return { movies: mappedMovies }
-}
+import { useMovies } from './hooks/useMovies'
 
 function App () {
   const { movies: mappedMovies } = useMovies()
