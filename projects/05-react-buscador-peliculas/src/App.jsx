964ece25364f8d1cd91ff54894be362b936ce1a3
sebasbad <sebasbad@gmail.com>
2024-07-31 18:50:23 +0200

    Updated app to use loading

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -46,7 +46,7 @@ function useSearch () {
 
 function App () {
   const { search, setSearch, error } = useSearch()
-  const { movies, getMovies } = useMovies({ search })
+  const { movies, getMovies, loading } = useMovies({ search })
 
   console.log('render') //
 
@@ -86,7 +86,9 @@ function App () {
         {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
-        <Movies movies={movies} />
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
       </main>
     </div>
   )
