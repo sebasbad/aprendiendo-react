a39bb5c51addf710f615a852ee7211756873bed4
sebasbad <sebasbad@gmail.com>
2024-07-31 18:44:12 +0200

    Updated use movies hook to add loading and errors management

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -3,11 +3,22 @@ import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const getMovies = async () => {
-    const newMovies = await searchMovies({ search })
-    setMovies(newMovies)
+    try {
+      setLoading(true)
+      setError(null)
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+    } catch (e) {
+      setError(e.mssage)
+    } finally {
+      // tanto en el try como en el catch
+      setLoading(false)
+    }
   }
 
-  return { movies, getMovies }
+  return { movies, getMovies, loading, error }
 }
