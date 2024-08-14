19dd6895b2cabb8903ff7bd6319643c709487529
sebasbad <sebasbad@gmail.com>
2024-08-14 20:47:29 +0200

    Updated icons and products components to format code

--- projects/06-shopping-cart/src/components/Products.jsx
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -1,29 +1,28 @@
 import './Products.css'
 import { AddToCartIcon } from './Icons'
 
-export function Products({ products }) {
+export function Products ({ products }) {
+  return (
+    <main className='products'>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+            />
+            <div>
+              <strong>{product.title}</strong>
+            </div>
+            <div>
+              <button>
+                <AddToCartIcon />
+              </button>
+            </div>
 
-    return (
-        <main className='products'>
-            <ul>
-                {products.map(product => {
-                    <li key={product.id}>
-                        <img
-                            src={product.thumbnail}
-                            alt={product.title}
-                        />
-                        <div>
-                            <strong>{product.title}</strong>
-                        </div>
-                        <div>
-                            <button>
-                                <AddToCartIcon />
-                            </button>
-                        </div>
-
-                    </li>
-                })}
-            </ul>
-        </main>
-    )
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
 }
