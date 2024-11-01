ac166dad3a29fa04168284ecfb6beb16eb0e0f83
sebasbad <sebasbad@gmail.com>
2024-11-01 16:34:05 +0100

    Extracted use filters hook to a file

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,28 +1,10 @@
-import { useState, useContext } from 'react'
+import { useState } from 'react'
 import { Products } from './components/Products.jsx'
 import { products as initialProducts } from './mocks/products.json'
 import { Header } from './components/Header.jsx'
 import { Footer } from './components/Footer.jsx'
 import { IS_DEVELOPMENT } from './config.js'
-import { FiltersContext } from './context/filters.jsx'
-
-function useFilters () {
-  const { filters, setFilters } = useContext(FiltersContext)
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          filters.category === product.category
-        )
-      )
-    })
-  }
-
-  return { filters, filterProducts, setFilters }
-}
+import { useFilters } from './hooks/useFilters.js'
 
 function App () {
   const [products] = useState(initialProducts)
