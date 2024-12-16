00457082a29581608c48effb2822087a11c5c048
sebasbad <sebasbad@gmail.com>
2024-12-16 19:35:25 +0100

    Updated products to show remove from cart for products already in cart

--- projects/06-shopping-cart/src/components/Products.jsx
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -1,30 +1,43 @@
 import './Products.css'
-import { AddToCartIcon } from './Icons'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons'
 import { useCart } from '../hooks/useCart'
 
 export function Products ({ products }) {
-  const { addToCart } = useCart()
+  const { cart, addToCart } = useCart()
+
+  const checkProductInCart = product => {
+    return cart.some(item => item.id === product.id)
+  }
 
   return (
     <main className='products'>
       <ul>
-        {products.slice(0, 10).map(product => (
-          <li key={product.id}>
-            <img
-              src={product.thumbnail}
-              alt={product.title}
-            />
-            <div>
-              <strong>{product.title}</strong> - ${product.price}
-            </div>
-            <div>
-              <button onClick={() => addToCart(product)}>
-                <AddToCartIcon />
-              </button>
-            </div>
+        {products.slice(0, 10).map(product => {
+          const isProductInCart = checkProductInCart(product)
 
-          </li>
-        ))}
+          return (
+            <li key={product.id}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
+              <div>
+                <button onClick={() => addToCart(product)}>
+                  {
+                    isProductInCart
+                      ? <RemoveFromCartIcon />
+                      : <AddToCartIcon />
+                  }
+
+                </button>
+              </div>
+
+            </li>
+          )
+        })}
       </ul>
     </main>
   )
