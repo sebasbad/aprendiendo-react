1f155786ab12f34ff0ac56a0b4c73b7c85430d76
sebasbad <sebasbad@gmail.com>
2024-12-16 19:00:33 +0100

    Updated cart context to implement add to cart increasing product quantity

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -8,8 +8,27 @@ export function CartProvider ({ children }) {
   const [cart, setCart] = useState([])
 
   const addToCart = product => {
-    setCart([...cart, product])
+    // check if product already in the cart
+    const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+    if (productInCartIndex >= 0) {
+      // producto ya está en el carritp
+      // una forma sería usando structuredClone
+      const newCart = structuredClone(cart)
+      newCart[productInCartIndex].quantity += 1
+      return setCart(newCart)
+    }
+
+    // producto no está en el carritp
+    setCart(prevState => ([
+      ...prevState,
+      {
+        ...product,
+        quantity: 1
+      }
+    ]))
   }
+
   const clearCart = () => {
     setCart([])
   }
