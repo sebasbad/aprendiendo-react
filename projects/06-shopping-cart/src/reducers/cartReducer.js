16aa12dc49b112e3a9c12701bddb9403fd2c7e78
sebasbad <sebasbad@gmail.com>
2024-12-20 12:21:45 +0100

    Updated cart reducer implementation to avoid using switch statements

--- projects/06-shopping-cart/src/reducers/cartReducer.js
+++ projects/06-shopping-cart/src/reducers/cartReducer.js
@@ -11,44 +11,47 @@ export const updateLocalStorage = state => {
   window.localStorage.setItem('cart', JSON.stringify(state))
 }
 
+const UPDATE_STATE_BY_ACTION = {
+  [CART_REDUCER_ACTIONS.ADD_TO_CART]: (state, actionPayload) => {
+    const { id } = actionPayload
+    // check if product already in the cart
+    const productInCartIndex = state.findIndex(item => item.id === id)
+
+    if (productInCartIndex >= 0) {
+      // producto ya está en el carritp
+      // una forma sería usando structuredClone
+      const newState = structuredClone(state)
+      newState[productInCartIndex].quantity += 1
+      updateLocalStorage(newState)
+      return newState
+    }
+
+    const newState = [
+      ...state,
+      {
+        ...actionPayload, // product
+        quantity: 1
+      }
+    ]
+
+    updateLocalStorage(newState)
+    return newState
+  },
+  [CART_REDUCER_ACTIONS.REMOVE_FROM_CART]: (state, actionPayload) => {
+    const { id } = actionPayload
+    const newState = state.filter(item => item.id !== id)
+    updateLocalStorage(newState)
+    return newState
+  },
+  [CART_REDUCER_ACTIONS.CLEAR_CART]: () => {
+    updateLocalStorage(cartInitialState)
+    return cartInitialState
+  }
+}
+
 export const cartReducer = (state, action) => {
   const { type: actionType, payload: actionPayload } = action
-  switch (actionType) {
-    case CART_REDUCER_ACTIONS.ADD_TO_CART: {
-      const { id } = actionPayload
-      // check if product already in the cart
-      const productInCartIndex = state.findIndex(item => item.id === id)
 
-      if (productInCartIndex >= 0) {
-        // producto ya está en el carritp
-        // una forma sería usando structuredClone
-        const newState = structuredClone(state)
-        newState[productInCartIndex].quantity += 1
-        updateLocalStorage(newState)
-        return newState
-      }
-
-      const newState = [
-        ...state,
-        {
-          ...actionPayload, // product
-          quantity: 1
-        }
-      ]
-
-      updateLocalStorage(newState)
-      return newState
-    }
-    case CART_REDUCER_ACTIONS.REMOVE_FROM_CART: {
-      const { id } = actionPayload
-      const newState = state.filter(item => item.id !== id)
-      updateLocalStorage(newState)
-      return newState
-    }
-    case CART_REDUCER_ACTIONS.CLEAR_CART: {
-      updateLocalStorage(cartInitialState)
-      return cartInitialState
-    }
-  }
-  return state
+  const updateState = UPDATE_STATE_BY_ACTION[actionType]
+  return updateState ? updateState(state, actionPayload) : state
 }
